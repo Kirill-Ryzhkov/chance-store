c306a353e8e4306c88a8f85d6624966728eb8200
Kirill-Ryzhkov <slusc10a@gmail.com>
2025-01-13 14:56:01 -0500

    Update product card

--- src/components/Home/ProductTile.jsx
+++ src/components/Home/ProductTile.jsx
@@ -3,7 +3,7 @@ export default function ProductTile ({ item }) {
     const IMAGE_URL = process.env.REACT_APP_IMAGE_SERVER;
 
     return (
-        <div className="bg-background text-colorPrimary shadow-custom rounded-lg p-4 flex flex-col items-center">
+        <div className="bg-background text-colorPrimary shadow-custom rounded-lg p-4 flex flex-col items-center text-center">
             <div className="w-full xl:h-52 md:h-44 h-36 overflow-hidden rounded-md mb-2">
                 <img src={IMAGE_URL + item.image} alt={item.name} className="w-full xl:h-52 md:h-40 h-36 object-cover rounded-md mb-2" />
             </div>
